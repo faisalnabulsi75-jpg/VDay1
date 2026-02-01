797b10276df6bd2af21f25544b07352cf9a99bd9
faisalnabulsi75-jpg <faisalnabulsi75@gmail.com>
2026-02-01 03:43:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
       yesButton.style.fontSize = `${fontSize}px`;
   
       //no button text
-      const messages = ["No","Are you sure?","Mommyy please?","Don't do this to me :(","Say yes or else...",];
+      const messages = ["No","Mommy?","Mommyy please?","Don't do this to your baby :(","Say yes I beg",];
   
       if (noClickCount === 4) {
         const newButton = document.createElement("button");
@@ -92,7 +92,7 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
     valentineQuestion.innerHTML = `
       <img src="./images/image7.gif" alt="Celebration duckie" style="display: block; margin: 0 auto; width: 200px; height: auto;"/>
       Congratulations!!<br>
-      <span style="font-size: 20px; color: #bd1e59;">You have scored a baddie for Valentine's Day! <3</span>
+      <span style="font-size: 20px; color: #bd1e59;">YOU'RE MINE for Valentine's Day! (and after i hope) <3</span>
     `;
     valentineQuestion.style.textAlign = "center"; 
   
@@ -156,3 +156,4 @@ const getRandomNumber = (num) => {return Math.floor(Math.random() * (num + 1));}
   }
 
   
+
